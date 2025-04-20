72cacd0dc759350d73132beedd3a29a948862c20
imfelipegomes <felipe.agomesdelima@gmail.com>
2025-04-19 22:47:31 -0300

    Finalização do projeto de busca de filmes em PHP. Criando sistema simples para localizar e colocar em ordem, além de mensagem de não encontrado se digitado filme que não está no banco de dados e parâmetro de porcentagem % para identificar o filme se colocar alguma palavra correspondente, mesmo que não pesquise pelo nome corretamente e completo

--- busca.php
+++ busca.php
@@ -3,4 +3,38 @@ if (!isset($_GET['nome_filme'])) {
   header("Location: index.php");
   exit;
 }
+
+$nome = "%".trim($_GET['nome_filme'])."%";
+
+$dbh = new PDO('mysql:host=127.0.0.1;dbname=buscaphp', 'root', 'imfelipegomes');
+
+$sth = $dbh->prepare('SELECT * FROM `filmes` WHERE `nome` LIKE :nome');
+$sth->bindParam(':nome', $nome, PDO::PARAM_STR);
+$sth->execute();
+
+$resultados = $sth->fetchAll(PDO::FETCH_ASSOC);
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <title>Resultado da busca</title>
+</head>
+<body>
+  <h2>Resultado da busca</h2>
+  <?php
+  if (count($resultados)) {
+    foreach($resultados as $Resultado) {
+  ?>
+    <label><?php echo $Resultado['id']; ?> - <?php echo $Resultado['nome']; ?></label>
+    <br>
+  <?php
+    }
+  } else {
+  ?>
+    <label>Não foram encontrados resultados pelo termo buscado.</label>
+  <?php
+  }
+  ?>
+</body>
+</html>
