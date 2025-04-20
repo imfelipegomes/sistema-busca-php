24690ce724d3262fc155608fc06df68c0eb655dd
imfelipegomes <felipe.agomesdelima@gmail.com>
2025-04-19 21:52:21 -0300

    Criando sistema de busca em php, já criado os dois arquivos, e banco de dados mysql

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Busca PHP - imfelipegomes</title>
+</head>
+<body>
+    <h2>Pesquisa de Filmes</h2>
+    <form action="busca.php" method="GET">
+      <label>Nome do filme</label>
+      <input type="text" name="nome_filme" size="50" placeholder="Insira o nome do filme">
+      <button style="width:100px;">Buscar</button>
+    </form>
+</body>
+</html>
